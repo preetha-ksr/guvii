934d5181502206f1a5d55b5c87db35f3bf4ebee0
preetha-ksr <preethamurugan91097@gmail.com>
2017-06-05 10:30:32 -0700

    Create pnz.java

---
+++ pnz.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+class pnz
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a==0)
+{
+System.out.println("Zero");
+}
+else if(a>=0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("Negative");
+}
+}
+}
