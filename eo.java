612ac81cabe564a8e13f6468c7ff46df7064ea97
preetha-ksr <preethamurugan91097@gmail.com>
2017-06-05 10:39:43 -0700

    Create eo.java

---
+++ eo.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+class eo
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
